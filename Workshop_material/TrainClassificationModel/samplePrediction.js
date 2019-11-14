258eb8a3b89f01f64aa9b04efbec3a61ed8d94dd
b05303009 <55622479+b05303009@users.noreply.github.com>
2019-11-14 15:03:42 +0800

    Update samplePrediction.js

--- Workshop_material/TrainClassificationModel/samplePrediction.js
+++ Workshop_material/TrainClassificationModel/samplePrediction.js
@@ -1,11 +1,11 @@
 const util = require('util');
 const fs = require('fs');
 const PredictionApiClient = require("@azure/cognitiveservices-customvision-prediction");
-const predictionKey = "cfc4cf193dd149cd8956ac7703398481";
-const sampleDataRoot = "/Users/yuhsuan/YuHsuan/Microsoft/customvision/Workshop_material/TrainClassificationModel/images";
-const endPoint = "https://sampleclassificati-prediction.cognitiveservices.azure.com/";
-const publishIterationName = "classifyModel";
-const projectId = "cf249461-e0d9-44e6-81c4-a5cfefa836bb"
+const predictionKey = "<>";
+const sampleDataRoot = "<>";
+const endPoint = "<>";
+const publishIterationName = "<>";
+const projectId = "<>"
 
 
 var testImage = process.argv.slice(2);
